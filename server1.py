3b648c2939060162432b706eef54c25b6742c3dd
MikailCeran <ceran123@hotmail.com>
2023-12-07 19:25:19 +0100

    jj

--- server1.py
+++ server1.py
@@ -1,22 +1,20 @@
+import numpy
 import sounddevice as sd
-import numpy as np
 from flask import Flask, jsonify
-from flask_cors import CORS
+import time
 
 app = Flask(__name__)
-CORS(app)
 
 def capture_decibels():
-    duration = 1  # seconds
+    duration = 1
     samplerate = 44100
 
     # Capture audio
-    audio_data = sd.rec(int(samplerate * duration), samplerate=samplerate, channels=1, dtype='int16')
-    sd.wait()
+    audio_data, _ = sd.read(samplerate=samplerate, channels=1, dtype='int16', duration=duration)
 
-    # Calculate decibel level
-    rms = np.sqrt(np.mean(np.square(audio_data)))
-    decibel_data = 20 * np.log10(rms)
+    # Calculate decibels
+    rms = numpy.sqrt(numpy.mean(audio_data**2))
+    decibel_data = 20 * numpy.log10(rms)
 
     return decibel_data
 

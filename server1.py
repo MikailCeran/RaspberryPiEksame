dd225ef7627c21727d8c47f21b3456fd12107b86
MikailCeran <ceran123@hotmail.com>
2023-12-07 19:31:27 +0100

    k

--- server1.py
+++ server1.py
@@ -1,26 +1,29 @@
 import numpy
 import sounddevice as sd
 from flask import Flask, jsonify
-from flask_cors import CORS  # Import the CORS module
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
 
 def capture_decibels():
     duration = 1  # seconds
     samplerate = 44100
 
+    # Capture audio
     audio_data, _ = sd.read(samplerate=samplerate, channels=1, dtype='int16', duration=duration)
+
+    # Calculate decibel level using numpy
     rms = numpy.sqrt(numpy.mean(audio_data**2))
     decibel_data = 20 * numpy.log10(rms)
 
-    print(f"Decibel data: {decibel_data} dB")
     return decibel_data
 
 @app.route('/get_decibels', methods=['GET'])
 def get_decibels():
-    decibel_data = capture_decibels()
-    return jsonify({"decibel_data": decibel_data})
+    try:
+        decibel_data = capture_decibels()
+        return jsonify({"decibel_data": decibel_data}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, threaded=True)

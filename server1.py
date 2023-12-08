e7e94d16b60cd5d42bf9963bc771727400d9ec35
MikailCeran <ceran123@hotmail.com>
2023-12-08 20:44:48 +0100

    s

--- server1.py
+++ server1.py
@@ -13,6 +13,7 @@ CORS(app)
 
 # Global variables for storing decibel readings and timestamps
 decibel_readings = []
+decibels_data = {"average_pr_1min": [], "average_pr_10min": []}
 
 # Lock for thread safety when updating global variables
 lock = threading.Lock()
@@ -56,9 +57,43 @@ def capture_audio():
         # Terminate the PyAudio instance
         p.terminate()
 
+# Route to get decibel data directly from the JSON file
+@app.route('/get_decibels_data', methods=['GET'])
+def get_decibels_data():
+    try:
+        with lock:
+            return jsonify(decibels_data)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+# Route for the index page
+@app.route('/')
+def index():
+    return "Hello, this is the index page!"
+
+# Route to get decibels using the previous logic
+@app.route('/get_decibels', methods=['GET'])
+def get_decibels():
+    from tasks.tasks import calculate_and_send_decibels
+    try:
+        with lock:
+            audio_data = capture_audio()
+            decibels = calculate_and_send_decibels(audio_data)
+            return jsonify({"decibels": decibels["decibels"]})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+# Route to get captured audio file
+@app.route('/captured_audio', methods=['GET'])
+def get_captured_audio():
+    try:
+        return send_file('captured_audio.wav', as_attachment=True)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
 # Periodic task to calculate average decibels every 1 minute
 def calculate_average_decibels_1min():
-    global decibel_readings
+    global decibel_readings, decibels_data
 
     with lock:
         try:
@@ -76,8 +111,9 @@ def calculate_average_decibels_1min():
             decibel_readings = decibel_readings[-10:]
 
             # Update the JSON file
+            decibels_data["average_pr_1min"] = [(average_decibel, timestamp)]
             with open('decibels_data.json', 'w') as json_file:
-                json.dump({"average_pr_1min": [(average_decibel, timestamp)]}, json_file)
+                json.dump(decibels_data, json_file)
 
         except Exception as e:
             print(f"Error in calculate_average_decibels_1min: {e}")
@@ -87,19 +123,20 @@ def calculate_average_decibels_1min():
 
 # Periodic task to calculate average decibels every 10 minutes
 def calculate_average_decibels_10min():
-    global decibel_readings
+    global decibel_readings, decibels_data
 
     with lock:
         try:
             # Calculate the average decibel level for the past 10 minutes
             if decibel_readings:
                 average_10min = np.mean([reading[0] for reading in decibel_readings])
+                decibels_data["average_pr_10min"].append((average_10min, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+                # Trim the list to keep only the last 10 minutes of readings
+                decibel_readings = decibel_readings[-10:]
+
                 # Update the JSON file
-                with open('decibels_data.json', 'r') as json_file:
-                    data = json.load(json_file)
-                    data["average_pr_10min"].append((average_10min, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
                 with open('decibels_data.json', 'w') as json_file:
-                    json.dump(data, json_file)
+                    json.dump(decibels_data, json_file)
 
         except Exception as e:
             print(f"Error in calculate_average_decibels_10min: {e}")
@@ -107,10 +144,10 @@ def calculate_average_decibels_10min():
     # Schedule the task to run again in 10 minutes
     threading.Timer(600, calculate_average_decibels_10min).start()
 
-# Run the Flask app
 if __name__ == "__main__":
     # Start the threads for periodic tasks
     threading.Timer(60, calculate_average_decibels_1min).start()
     threading.Timer(600, calculate_average_decibels_10min).start()
 
+    # Run the Flask app
     app.run(host='0.0.0.0', port=8080, threaded=True)

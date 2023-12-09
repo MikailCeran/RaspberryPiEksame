03aa16b05936cc19b92c10c9ee065626d98d4836
MikailCeran <ceran123@hotmail.com>
2023-12-09 20:45:44 +0100

    s

--- server1.py
+++ server1.py
@@ -21,40 +21,37 @@ lock = threading.Lock()
 # Function to capture audio from the microphone using PyAudio
 def capture_audio():
     try:
-        # Initialize PyAudio outside the try block
         p = pyaudio.PyAudio()
 
-        # Set the sampling parameters
-        duration = 1  # seconds
-        samplerate = 44100
-        channels = 1  # 1 for mono, 2 for stereo
+        # Get default input device
+        input_device_index = p.get_default_input_device_info()['index']
 
-        # Open a stream for audio input using a context manager
-        with p.open(
-            format=pyaudio.paInt16,
-            channels=channels,
-            rate=samplerate,
-            input=True,
-            frames_per_buffer=samplerate
-        ) as stream:
-            # Read audio data from the stream
-            frames = []
-            for i in range(int(samplerate / duration)):
-                data = stream.read(int(samplerate * duration / (samplerate / duration)))
-                frames.append(np.frombuffer(data, dtype=np.int16))
+        stream = p.open(format=pyaudio.paInt16,
+                        channels=1,
+                        rate=44100,
+                        input=True,
+                        input_device_index=input_device_index,
+                        frames_per_buffer=1024)
 
-        # Convert the frames to a NumPy array
-        audio_data = np.concatenate(frames)
+        print("Recording...")
 
+        frames = []
+        for i in range(0, int(44100 / 1024 * 5)):  # Adjust the number of frames based on your needs
+            data = stream.read(1024)
+            frames.append(data)
+
+        print("Finished recording.")
+
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+        audio_data = np.frombuffer(b''.join(frames), dtype=np.int16)
         return audio_data
 
     except Exception as e:
         print(f"Error in capture_audio: {e}")
         return {"error": str(e)}
-    finally:
-        # Terminate the PyAudio instance in the finally block
-        if 'p' in locals() and hasattr(p, 'terminate'):
-            p.terminate()
 
 # Route to get decibel data directly from the JSON file
 @app.route('/get_decibels_data', methods=['GET'])
@@ -62,23 +59,6 @@ def get_decibels_data():
     try:
         with lock:
             return jsonify(decibels_data)
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-# Route for the index page
-@app.route('/')
-def index():
-    return "Hello, this is the index page!"
-
-# Route to get decibels using the previous logic
-@app.route('/get_decibels', methods=['GET'])
-def get_decibels():
-    from tasks.tasks import calculate_and_send_decibels
-    try:
-        with lock:
-            audio_data = capture_audio()
-            decibels = calculate_and_send_decibels(audio_data)
-            return jsonify({"decibels": decibels["decibels"]})
     except Exception as e:
         return jsonify({"error": str(e)})
 
